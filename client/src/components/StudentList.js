a9339130f846158dc1d68da43e8e2139b64a34f3
Jose Fletes <joseangelfletes@gmail.com>
2023-02-20 12:21:57 -0800

    added css to student list table

--- client/src/components/StudentList.js
+++ client/src/components/StudentList.js
@@ -39,7 +39,7 @@ const StudentList = ({ students }) => {
 
 
   return (
-    <div>
+    <div >
       <h1>Student List</h1>
       <table>
         <thead>
@@ -112,11 +112,11 @@ const StudentList = ({ students }) => {
         <tbody>
           {filteredStudents.map(student => (
             <tr key={student.id}>
-              <td><button onClick={() => {navigate(`/current-students/${student.id}`)}}>*</button>{student.first_name} {student.last_name}</td>
+              <td><button onClick={() => {navigate(`/current-students/${student.id}`)}}>🔎</button> {student.first_name} {student.last_name}</td>
               <td>{student.age}</td>
               <td>{student.interest}</td>
               <td>{student.student_since}</td>
-              <td><button onClick={() => {navigate(`/current-user/${student.user.id}`)}}>*</button>{student.user.first_name} {student.user.last_name}</td>
+              <td><button onClick={() => {navigate(`/current-user/${student.user.id}`)}}>🔎</button> {student.user.first_name} {student.user.last_name}</td>
               <td>{student.user.email}</td>
               <td>{student.user.phone_number}</td>
             </tr>
