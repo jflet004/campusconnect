9126ed69710acec74158e84d6b8745f3be99b225
Jose Fletes <joseangelfletes@gmail.com>
2023-02-19 16:47:36 -0800

    add login & logout components

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,13 +1,27 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { NavLink, useNavigate } from 'react-router-dom'
+import { UserContext } from '../context/user'
 
 const NavBar = () => {
+
+  const {currentUser, setCurrentUser} = useContext(UserContext)
+  const navigate = useNavigate()
+
+  const handleLogoutClick = () => {
+    fetch('/logout', {
+      method: "DELETE"
+    })
+    setCurrentUser(null)
+    navigate('/login')
+  }
+
   return (
     <div>
       <NavLink to="/">Home</NavLink>
       <NavLink to="/login">Login</NavLink>
       <NavLink to="/current-students">Current Students</NavLink>
       <NavLink to="/register-students">Register Students</NavLink>
+      <button onClick={handleLogoutClick}>Logout</button>
     </div>
   )
 }
