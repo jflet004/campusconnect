674f4f2adb7e0688a81c4b386ca05e4dcb78e6ae
Jose Fletes <joseangelfletes@gmail.com>
2023-03-12 16:54:10 -0700

    Add validations to User and Teacher models

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,4 +2,9 @@ class Teacher < ApplicationRecord
   has_many :teacher_assignments
   has_many :courses, through: :teacher_assignments
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :phone_number, length: { in: 10..15 }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
 
   validates :first_name, presence:true
   validates :last_name, presence:true
-  validates :email, presence:true, uniqueness:true
+  validates :email, uniqueness:true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :address, presence:true
   validates :city, presence:true
   validates :state, presence:true
